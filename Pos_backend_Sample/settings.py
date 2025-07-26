d35067f7df83614b04754cf21ba73aa3e55b9c6a
Hariharan <hariharanofficial08@gmail.com>
2025-07-26 23:53:25 +0530

    Update settings.py

--- Pos_backend_Sample/settings.py
+++ Pos_backend_Sample/settings.py
@@ -91,7 +91,7 @@ else:
     # Use environment variables for production database settings
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get("DB_NAME"),
             'USER': os.environ.get("DB_USER"),
             'PASSWORD': os.environ.get("DB_PASSWORD"),
